47a7fe3a216279dc21b337dfd1c5bfe9bd5c9003
David wanjohi <davidwagura83@gmail.com>
2024-06-16 20:38:37 +0300

    Create router and do file importation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+
+import '@/assets/index'
+
+
+
+const app  = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
