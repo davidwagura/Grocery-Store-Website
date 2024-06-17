5920d97ea6ed0a2039f79b2de5b6a52ab504514f
David wanjohi <davidwagura83@gmail.com>
2024-06-17 21:53:16 +0300

    Create products pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,35 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/components/HomePage.vue'
 
+import BakeryPage from '@/components/pages/BakeryPage.vue';
+
+import DairyProducts from '@/components/pages/DairyProducts.vue';
+
+import FruitsPage from '@/components/pages/FruitsPage.vue';
+
+import MeatSeafood from '@/components/pages/Meat-Seafood.vue';
+
+import PantryStaples from '@/components/pages/PantryStaples.vue';
+
+import VegetablePage from '@/components/pages/VegetablePage.vue';
+
 
 const routes = [
 
-{path: '/', component: HomePage }
+{ path: '/', component: HomePage },
+
+{ path: '/fruits', component: FruitsPage },
+
+{ path: '/vegetables', component: VegetablePage },
+
+{ path: '/dairy', component: DairyProducts },
+
+{ path: '/meat-seafood', component: MeatSeafood },
+
+{ path: '/bakery', component: BakeryPage },
+
+{ path: '/pantry', component: PantryStaples },
+
 
 ]
 
